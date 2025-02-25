306d28e1d6641d8957018f436536701c0d8c360f
{ROA-DEV} <105471756+tcer12@users.noreply.github.com>
2025-02-25 17:41:33 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const myImage = document.getElementById('myImage');
+
+myImage.classList.add('pulse');
+
+myImage.addEventListener('click', () => {
+    window.location.href = 'introduccion.html'; // Replace with your file name
+});
